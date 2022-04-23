052ef32674f61891bb83a9f69a886e60c7dbaf06
ErickSchuenck <esfmokdeci@gmail.com>
2022-04-23 14:28:53 -0300

    versão final

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from 'cors'
+import chalk from "chalk";
+const app = express();
+
+const holidays = [
+  { date: "1/1/2022", name: "Confraternização mundial" },
+  { date: "1/3/2022", name: "Carnaval" },
+  { date: "4/17/2022", name: "Páscoa" },
+  { date: "4/21/2022", name: "Tiradentes" },
+  { date: "5/1/2022", name: "Dia do trabalho" },
+  { date: "6/16/2022", name: "Corpus Christi" },
+  { date: "9/7/2022", name: "Independência do Brasil" },
+  { date: "10/12/2022", name: "Nossa Senhora Aparecida" },
+  { date: "11/2/2022", name: "Finados" },
+  { date: "11/15/2022", name: "Proclamação da República" },
+  { date: "12/25/2022", name: "Natal" }
+];
+
+app.listen(5001, () => {
+  console.log(chalk.bold.green('App is Working!!!'))
+})
+
+
+
+app.get('/holidays', (req, res) => {
+  res.send(holidays)
+})
+
+app.get('/is-today-holiday', (req, res) => {
+
+  const hoje = new Date();
+  hoje.toLocaleDateString("en-us")
+  const feriado = holidays.find(holiday => {
+    return holiday.date === hoje;
+  })
+
+  if (feriado == !undefined) { res.send(`Sim, hoje é feriado de ${feriado.name}`) }
+  else { res.send("Não, hoje não é feriado") }
+})
+
+app.use(cors())
